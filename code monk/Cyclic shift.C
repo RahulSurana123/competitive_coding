95d44550152a517588f43aa65147cc542ffa97a9
RahulSurana123 <rasuru04@gmail.com>
2021-01-16 19:07:55 +0530

    Some new updates

---
+++ code monk/Cyclic shift.C
@@ -0,0 +1,27 @@
+
+/* 
+
+    Solution by Rahul Surana
+
+    ***********************************************************
+
+A large binary number is represented by a string  of size A and N comprises of 0s and 1s. 
+You must perform a cyclic shift on this string. The cyclic shift operation is defined as follows:
+
+If the string A is [a0,a1,a2,...,a(n-1)], then after performing one cyclic shift, the string becomes [a1,a2,a3,...,a0].
+
+You performed the shift infinite number of times and each time you recorded the value of the binary number represented by the string. 
+The maximum binary number formed after performing (possibly 0) the operation is B. 
+Your task is to determine the number of cyclic shifts 
+that can be performed such that the value represented by the string A will be equal to B for the kth time.
+
+Input format
+
+First line: A single integer T denoting the number of test cases
+For each test case:
+First line: Two space-separated integers N and K 
+Second line: A denoting the string
+
+    ***********************************************************
+*/
+
