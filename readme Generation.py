f0a74a37c664d18ab2015544288023616f3e28c5
RahulSurana123 <rasuru04@gmail.com>
2021-11-02 23:08:24 +0530

    Made new readme generation technic

--- readme Generation.py
+++ readme Generation.py
@@ -38,7 +38,7 @@ for dirs in list(os.listdir(".")):
 		dic[dirs] = ("./"+"%20".join(list(dirs.split(" "))))
 		x = f"[ {dirs} ]({dic[dirs]})"
 		y = f"Problem Count:  {str(len(list(os.listdir(dirs))))}"
-		z = 75 - len(x)
+		z = 50 - len(dirs)
 		s = f"-  {x:}   {''.join(['&nbsp;' for i in range(z)])}   {y}\n"
 		print(s)
 		f.write(s)
