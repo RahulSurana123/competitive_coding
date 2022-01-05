02caa92b97549ca0346465bb7ff3e1fa66e2d755
RahulSurana123 <rasuru04@gmail.com>
2022-01-05 17:43:44 +0530

    improving daily leetcode code

--- leet code/Palindrome Partitioning.cpp
+++ leet code/Palindrome Partitioning.cpp
@@ -27,21 +27,22 @@ public:
         return true;
     }
     
-    void dff(int i, string s, vector<string> x){
-        if(i==s.length()){
+    void dff(int i, string& s, vector<string>& x){
+        if(i>=s.length()){
             ans.push_back(x);
         }
         for(int k = i; k < s.length(); k++){
             if(checkpalin(s.substr(i,k-i+1))){
-                vector<string> z(x.begin(),x.end());
+                // vector<string> z(x.begin(),x.end());
                 cout << s.substr(i,k-i+1) <<" ";
-                z.push_back(s.substr(i,k-i+1));
-                dff(k+1,s,z);
+                x.push_back(s.substr(i,k-i+1));
+                dff(k+1,s,x);
+                x.pop_back();
             }
         }
     }
     
-    void df(string s){
+    void df(string& s){
         for(int i = 0; i < s.length(); i++){
             if(checkpalin(s.substr(0,i+1))){
                 vector<string> x;
