1c1cd570ceb73791e31eaa52ce2651468fcdaf98
RahulSurana123 <rasuru04@gmail.com>
2022-01-06 21:00:58 +0530

    updating daily leetcode code

---
+++ leet code/Car Pooling.cpp
@@ -0,0 +1,37 @@
+
+
+/* 
+    Solution by Rahul Surana
+    
+    ***********************************************************
+
+
+There is a car with capacity empty seats. The vehicle only drives east (i.e., it cannot turn around and drive west).
+
+You are given the integer capacity and an array trips where trip[i] = [numPassengersi, fromi, toi] indicates that the ith trip has numPassengersi passengers and the locations to pick them up and drop them off are fromi and toi respectively. The locations are given as the number of kilometers due east from the car's initial location.
+
+Return true if it is possible to pick up and drop off all passengers for all the given trips, or false otherwise.
+
+
+    ***********************************************************
+*/
+
+
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        vector<vector<int>> v;
+        for(auto x: trips){
+            v.push_back({x[1],1,x[0]});
+            v.push_back({x[2],0,x[0]});
+        }
+        sort(v.begin(),v.end());
+        int s = 0;
+        for(auto x: v){
+            if(x[1] == 1) s+=x[2];
+            else s-=x[2];
+            if(s>capacity) return false;
+        }
+        return true;
+    }
+};
