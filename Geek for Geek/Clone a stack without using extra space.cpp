d54e1d5a5eea776d651af7c42465286918cf703e
RahulSurana123 <rasuru04@gmail.com>
2022-01-11 22:59:07 +0530

    updating geek for geek daily code

---
+++ Geek for Geek/Clone a stack without using extra space.cpp
@@ -0,0 +1,78 @@
+
+
+
+
+/* 
+    Solution by Rahul Surana
+    
+    ***********************************************************
+
+
+Given elements of a stack, clone the stack without using extra space.
+
+
+
+    ***********************************************************
+*/
+
+
+// { Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    void clonestack(stack<int> st, stack<int>& cloned)
+    {
+        if(st.empty()) return;
+        int t = st.top();
+        st.pop();
+        clonestack(st,cloned);
+        cloned.push(t);
+    }
+};
+
+// { Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N;
+        cin >> N;
+        int arr[N];
+        stack<int> st;
+        vector<int> copy;
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+            st.push(arr[i]);
+            copy.push_back(arr[i]);
+        }
+        
+        reverse(copy.begin(), copy.end());
+        
+        Solution ob;
+        stack<int> cloned;
+        ob.clonestack(st,cloned);
+        vector<int> check;
+        while(cloned.size())
+        {
+            check.push_back(cloned.top());
+            cloned.pop();
+        }
+        
+        int flag = 0;
+        if(check!=copy)
+            flag = 1;
+        
+        cout<<1-flag<<endl;
+    }
+    return 0; 
+} 
+  // } Driver Code Ends
