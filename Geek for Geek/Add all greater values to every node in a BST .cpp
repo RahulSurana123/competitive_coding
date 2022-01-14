a296240623b656197f4be46a18bf427a36a44426
RahulSurana123 <rasuru04@gmail.com>
2022-01-14 17:11:33 +0530

    updating daily code geek for geek and readme generation file for url encoding

---
+++ Geek for Geek/Add all greater values to every node in a BST .cpp
@@ -0,0 +1,170 @@
+
+
+/* 
+    Solution by Rahul Surana
+    
+    ***********************************************************
+
+
+Given a BST, modify it so that all greater values in the given BST are added to every node.
+
+
+
+    ***********************************************************
+*/
+
+
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define MAX_HEIGHT 100000
+
+// Tree Node
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+
+
+Node* modify(Node *root);
+
+// Function to Build Tree
+Node* buildTree(string str)
+{
+   // Corner Case
+   if(str.length() == 0 || str[0] == 'N')
+       return NULL;
+
+   // Creating vector of strings from input
+   // string after spliting by space
+   vector<string> ip;
+
+   istringstream iss(str);
+   for(string str; iss >> str; )
+       ip.push_back(str);
+
+   // Create the root of the tree
+   Node* root = new Node(stoi(ip[0]));
+
+   // Push the root to the queue
+   queue<Node*> queue;
+   queue.push(root);
+
+   // Starting from the second element
+   int i = 1;
+   while(!queue.empty() && i < ip.size()) {
+
+       // Get and remove the front of the queue
+       Node* currNode = queue.front();
+       queue.pop();
+
+       // Get the current node's value from the string
+       string currVal = ip[i];
+
+       // If the left child is not null
+       if(currVal != "N") {
+
+           // Create the left child for the current node
+           currNode->left = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->left);
+       }
+
+       // For the right child
+       i++;
+       if(i >= ip.size())
+           break;
+       currVal = ip[i];
+
+       // If the right child is not null
+       if(currVal != "N") {
+
+           // Create the right child for the current node
+           currNode->right = new Node(stoi(currVal));
+
+           // Push it to the queue
+           queue.push(currNode->right);
+       }
+       i++;
+   }
+
+   return root;
+}
+
+void inorder(Node *root, vector<int> &v)
+{
+    if(root==NULL)
+        return;
+
+    inorder(root->left, v);
+    v.push_back(root->data);
+    inorder(root->right, v);
+}
+
+int main() {
+   
+   int t;
+   string tc;
+   getline(cin, tc);
+   t=stoi(tc);
+   while(t--)
+   {
+        string s; 
+       getline(cin, s);
+       Node* root = buildTree(s);
+
+       //getline(cin, s);
+        // int k = stoi(s);
+        // getline(cin, s);
+
+        root = modify(root);
+        vector<int> v;
+        inorder(root, v);
+        for(auto i:v)
+            cout << i << " ";
+        cout << endl;
+       //cout<<"~"<<endl;
+   }
+   return 0;
+}// } Driver Code Ends
+
+
+/*
+Node structure is as follows:
+struct Node {
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+*/
+// modify the BST and return its root
+
+void sum(Node* root, int &s){
+    if(root == NULL) return ;
+    sum(root->right, s);
+    root->data += s;
+    s = root->data;
+    sum(root->left, s);
+}
+
+Node* modify(Node *root)
+{
+    if(root == NULL) return root;
+    if(root->right == NULL && root->left == NULL) return root;
+    int x = 0;
+    sum(root,x);
+    return root;
+}
---
+++ Striver’s SDE Sheet/README.md
@@ -0,0 +1,21 @@
+# *Welcome To Coding Club*
+### <div style='text-align:right'><sub> - Rahul Surana</sub></div>
+### Going To Update This Repo Time To Time As I Continue My Journey To Competitive Programming
+### Please Do Star the Repo if it ever helps you. Also Would Like to Form a Community So we all can grow Together
+-  [ Whatsapp Group Link ](https://chat.whatsapp.com/FUV7H9SIBYKD3C5tgDluI6) 
+### HYPERLINK To Code
+***
+-  [ Best Time to Buy and Sell Stock ](.%2FBest%2520Time%2520to%2520Buy%2520and%2520Sell%2520Stock.cpp) 
+-  [ Find the Duplicate Number ](.%2FFind%2520the%2520Duplicate%2520Number.cpp) 
+-  [ Maximum Subarray ](.%2FMaximum%2520Subarray.cpp) 
+-  [ Merge Intervals ](.%2FMerge%2520Intervals.cpp) 
+-  [ Merge Sorted Array ](.%2FMerge%2520Sorted%2520Array.cpp) 
+-  [ Next Permutation ](.%2FNext%2520Permutation.cpp) 
+-  [ Pascal's Triangle ](.%2FPascal%27s%2520Triangle.cpp) 
+-  [ readme Generation ](.%2Freadme%2520Generation.py) 
+-  [ READM ](.%2FREADME.md) 
+-  [ Repeat and Missing Number Array ](.%2FRepeat%2520and%2520Missing%2520Number%2520Array.cpp) 
+-  [ Rotate Image ](.%2FRotate%2520Image.cpp) 
+-  [ Set Matrix Zeroes ](.%2FSet%2520Matrix%2520Zeroes.cpp) 
+-  [ Sort Colors ](.%2FSort%2520Colors.cpp) 
+### Total Code Count : 13
