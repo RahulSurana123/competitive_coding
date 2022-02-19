42c139e92c62dd2650534ed8ba177685dac039fa
RahulSurana123 <rasuru04@gmail.com>
2022-02-19 20:23:55 +0530

    updating juspay hiring round 2 solution

--- Hiring/Locking The Tree of Space.cpp
+++ Hiring/Locking The Tree of Space.cpp
@@ -185,181 +185,132 @@ Additional Notes:
 #define trace(x)    cerr<<#x<<" : "<<x<<endl;
 #define trace2(x,y) cerr<<#x<<" : "<<x<<" | "<<#y<<" : "<<y<<endl;
 #define trace3(x,y,z) cerr<<#x<<" : "<<x<<" | "<<#y<<" : "<<y<<" | "<<#z<<" : "<<z<<endl;
-#define fast_io 	std::ios::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
+#define fast_io     std::ios::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
  
 using namespace std;
 int inf=1e9+7, MOD=1e9+7;
 
-// vector<vector<int>> adj;
-map<int, Tree*> nToT;
-map<string, int> sToI;
-// vector<int> nuid;
-// vector<int> p;
+int w = 1;
 
-// vector<int> intime;
-// vector<int> outtime;
-// int timer = 0;
-
-// bool isPurvaj(int a,int b){
-//     return intime[a] > intime[b] && outtime[b] > outtime[a];
-// }
-
-// void dfs(int u){
-//     intime[u] = timer++; 
-//     for (auto x: adj[u]){
-//         dfs(x);
-//     }
-//     outtime[u] = timer++;
-// }
-
-// bool df(int u){
-//     if(nuid[u] != -1) return false;
-//     bool ans = true;
-//     for(auto x : adj[u]){
-//         ans = df(x);
-//         if(ans) break;
-//     }
-//     return ans;
-// }
-
-// void change(int u, int uuid){
-//     nuid[u] = uuid;
-//     for(auto x : adj[u]){
-//         change(x,uuid);
-//     }
-// }
-
-// bool lock(int nodeName,int uuid){
-//     if(nuid[nodeName] != -1) return false;
-//     bool ans = true;
-//     for(auto x : adj[nodeName]){
-//         // cout << x << "\n";
-//         ans = df(x);
-//         if(!ans) break;
-//     }
-//     if(ans){
-//         nuid[nodeName] = uuid;
-//         for(auto x : adj[nodeName]){
-//             change(x,uuid);
-//         }
-//     }
-//     return ans;
-// }
-
-// bool udf(int u, int uid){
-//     if(nuid[u] != uid) return false;
-//     bool ans = true;
-//     for(auto x : adj[u]){
-//         ans = udf(x,uid);
-//         if(!ans) break;
-//     }
-//     return ans;
-// }
-
-// bool unlock(int nn, int uuid){
-//     // cout << nuid[nn] << " " << nuid[p[nn]] <<" "<<uuid<<"\n"; 
-//     if(nuid[nn] != uuid || nuid[p[nn]] == uuid) return false;
-//     bool ans = true;
-//     for(auto x: adj[nn]) {
-//         ans = udf(x,uuid);
-//         if(!ans) break;
-//     }
-//     if(ans){
-//         nuid[nn] = -1;
-//         for(auto x: adj[nn]) {
-//             change(x,-1);
-//         }
-//     }
-//     return ans;
-// }
-
-// bool uudf(int u, int id){
-//     if(nuid[u] != -1 && nuid[u] != id) return false;
-//     bool ans = true;
-//     for(auto x: adj[u]) {
-//         ans = uudf(x,id);
-//         if(!ans) break;
-//     }
-//     return ans;
-// }
-
-// bool update(int nn, int uid){
-//     if((nuid[nn] != -1) || nuid[p[nn]] != -1) return false;
-//     bool ans = true;
-//     for(auto x: adj[nn]) {
-//         ans = uudf(x,uid);
-//         if(!ans) break;
-//     }
-//     if(df(nn)) ans = true; 
-//     if(ans){
-//         nuid[nn] = uid;
-//         for(auto x: adj[nn]) {
-//             change(x,uid);
-//         }
-//     }
-//     return ans;
-// }
-
-public class Tree{
+struct Tree{
     public :
-        bool l;
-        bool la;
-        int luid;
+        bool isLocked;
+        bool isLockable;
+        int id;
+        int ma;
         Tree* p;
         vector<Tree*> c; 
         Tree(){
-            l =false;
-            la=true;
-            luid = -1;
+            isLocked = false;
+            isLockable = true;
+            id = -1;
+            ma = w++;
             p = NULL;
         }
         Tree(Tree* parent){
-            l = false;
-            la=true;
-            luid = -1;
-            this->p = parent;
+            isLocked = false;
+            isLockable = true;
+            id = -1;
+            ma = w++;
+            p = parent;
         }
+};
+        // bool ll(Tree* n) return n->l;
 
-        bool ll(Tree* n) return n->l;
-
-        void locking(Tree* n, int uuid){
-            if(n->la == false || (n->la == true && n->luid != uuid) ) return;
+        bool locking(Tree* n, int uuid){
+            if(n->isLockable == false || (n->isLocked)) return false;
             Tree* T = n;
-            luid = uuid;
-            bool f = false;
-            while(T != NULL){
-                if(T->l == true){
-                    f = true;
-                    break;
-                }
-                T=T->p;
-            }
-            if(flag) return;
-            else{
-                n->l = true;
-                T = n;
-                while(T!=NULL){
-                    T->la = false;
-                    T->luid = uuid;
-                    T=T->p;
+            queue<Tree*> q;
+            q.push(T);
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    if(x->isLocked) return false;
+                    q.push(x);
                 }
             }
+            n->id = uuid;
+            n->isLocked = true;
+            q.push(T);
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    x->isLockable = false;
+                    q.push(x);
+                }
+            }
+            return true;
         }
         
-        void unlocking(Tree* n){
-            if(n -> l == false) return;
+        bool unlocking(Tree* n,int uuid){
+            if(n->isLockable == false || n -> isLocked == false || (n->isLocked && uuid != n->id)) return false;
             Tree* T = n;
-            n->luid = -1;
-            n->l=false;
-            while(T!=NULL){
-                T->la = true;
-                T->luid = -1;
-                T=T->p;
+            n->isLocked= false;
+            n->id = -1;
+            queue<Tree*> q;
+            q.push(T);
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    x->isLockable = true;
+                    q.push(x);
+                }
+            }
+            return true;
+        }
+
+        bool update(Tree* n,int uuid){
+            if(n->isLockable == false || n->isLocked == true) return false;
+            Tree* T = n;
+            queue<Tree*> q;
+            q.push(T);
+            bool f = false;
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    if(x->isLocked == true && x->id != uuid) { return false; }
+                    if(x->isLocked) f = true;
+                    q.push(x);
+                }
+            }
+            if(!f) return false;
+            // n->isLocked =  true;
+            // n->id = uuid;
+            // queue<Tree*> q;
+            q.push(T);
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    if(x->isLocked){
+                        // x->isLocked = false;
+                        if(!unlocking(x,uuid)) return false;
+                    }
+                    q.push(x);
+                }
+            }
+            // locking(n,uuid);
+            return locking(n,uuid);
+        }
+
+        void Print(Tree* n){
+            queue<Tree*> q;
+            q.push(n);
+            // cout << n->ma <<"\n";
+            while(!q.empty()){
+                Tree* temp = q.front();
+                q.pop();
+                for(auto x: temp->c){
+                    cout << temp->ma << " -> " << x->ma << " ";
+                }
             }
         }
-}
 
-
+unordered_map<string, Tree*> sToT;
 
 
 int main(){
@@ -382,21 +333,33 @@ int main(){
     //     FOR(j,p.size()) cout << j << " " <<p[j] <<" "; 
     //     cout <<"\n";
     // }
+    
+    Tree* t = new Tree(); 
     string d;
     cin >> d;
-    Tree* t = new Tree();
-    sToI.inser({s,1});
-    nToT.insert({1,t});
 
-    FOR(i,n) {
-        string s;
-        cin >> s;
-        sToI.insert({s,i+2});
-        Tree* x = new Tree(nToT[(int)(i+2)/m]);
-        nToT[(int)(i+2)/m].c.pb(x);
-        nToT.insert({i+2,x});
+    // a.c.push_back(&t);
+    sToT[d] = t;
+    queue<Tree*> queue;
+    queue.push(t);
+    int k = 1;
+    while(!queue.empty()){
+        auto temp = queue.front();
+        queue.pop();
+        // cout << temp->ma <<"\n";
+        while(k < n && (int)temp->c.size() < m){
+            string s;
+            cin >> s;
+            // cout << s <<"\n";
+            Tree* u = new Tree(temp);
+            sToT[s] = u;
+            temp->c.push_back(u);
+            queue.push(u);
+            k++;
+            // cout << s <<" "<<sToT[s]->ma <<"\n";
+        }
     }
-
+    // Print(t);
     // dfs(1);
     FOR(i,q) {
         int qn,uuid;
@@ -404,15 +367,15 @@ int main(){
         bool ans;
         cin >> qn >> name >> uuid;
         if(qn == 1){
-            ans = lock(nToi[name],uuid);
-            // cout << name<<" "<< nToi[name] << " " << uuid << "\n";
+            ans = locking(sToT[name],uuid);
+            // cout << name<<" "<< sToT[name]->id << " " << uuid << "\n";
         }
         else if(qn == 2){
-            ans = unlock(nToi[name],uuid);
-            // cout << name<<" "<< nToi[name] << " " << uuid << "\n";
+            ans = unlocking(sToT[name],uuid);
+            // cout << name<<" "<< sToT[name]->id << " " << uuid << "\n";
         }
         else {
-            ans = update(nToi[name],uuid);
+            ans = update(sToT[name],uuid);
             // cout << ans << "\n";
         }
         // FOR(j,nuid.size()) cout << j << " " << nuid[j] <<" ";
