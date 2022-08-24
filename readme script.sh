7fedfd2643ace90ae5ff8018afe391e632adff9f
Rahul Surana <rasuru04@gmail.com>
2022-08-24 15:23:12 +0530

    updating readme

--- readme script.sh
+++ readme script.sh
@@ -2,73 +2,75 @@
 
 echo "Started the ReadMe Generation"
 
-python "readme Generation.py"
+python3 "readme Generation.py"
+
+ls -a
 
 echo "Readme Generated for main folder"
 
-cd "code monk"
+cd "./code monk"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 cd ../CodeChef
 
 echo "Readme Generated for code monk folder"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 cd ../CodeForces
 
 echo "Readme Generated for CodeChef folder"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 cd "../Geek for Geek"
 
 echo "Readme Generated for CodeForces folder"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 cd "../hackerearth"
 
 echo "Readme Generated for Geek for Geek folder"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 cd "../hackerrank"
 
 echo "Readme Generated for hackerearth folder"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for hackerrank folder"
 
 cd "../Hiring"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for Hiring folder"
 
 cd "../leet code"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for leet code folder"
 
 cd "../Rated competition Codes"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for Rated competition Codes folder"
 
 cd "../Striver’s SDE Sheet"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for Striever SDE Sheet folder"
 
 cd "../Google Contest Problems"
 
-python "../readme Generation for folders.py"
+python3 "../readme Generation for folders.py"
 
 echo "Readme Generated for Google kickstart folder"
 
