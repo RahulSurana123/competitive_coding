b140e27806da53db24bdafb422d1c91a98fc929f
rahul <rasuru04@gmail.com>
2022-11-22 14:41:35 +0530

    updating readme

--- leet code/Perfect Squares.cpp
+++ leet code/Perfect Squares.cpp
@@ -1,5 +1,7 @@
 
 
+
+
 /* 
     Solution by Rahul Surana
     
@@ -9,26 +11,26 @@
 Given an integer n, return the least number of perfect square numbers that sum to n.
 
 A perfect square is an integer that is the square of an integer; in other words, 
-it is the product of some integer with itself. For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
+it is the product of some integer with itself. 
+For example, 1, 4, 9, and 16 are perfect squares while 3 and 11 are not.
 
-
+ 
 
     ***********************************************************
 */
 
 
-#include <bits/stdc++.h>
 
 class Solution {
 public:
     
-    
     int numSquares(int n) {
-        vector<int> sq(n+1);
-        for(int i = 1; i <= n; i++){
-            sq[i] = i;
-            for(int j = 2; j*j <=i ; j++){
-                sq[i] = min(sq[i],sq[i-j*j]+1);
+        int sq[n+1];
+        memset(sq,0,sizeof(sq));
+        for(int i = 1; i < n+1; i++){
+            sq[i]=i;
+            for(int j = 2; j*j <= i; j++){
+                sq[i] = min(sq[i-j*j]+1,sq[i]);
             }
         }
         return sq[n];
