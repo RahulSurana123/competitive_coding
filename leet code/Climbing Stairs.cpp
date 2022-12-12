a60ccf8fc50d8644c332e34da6bd24e718d968c3
rahul <rasuru04@gmail.com>
2022-12-12 11:31:07 +0530

    updating readme

--- leet code/Climbing Stairs.cpp
+++ leet code/Climbing Stairs.cpp
@@ -17,39 +17,19 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
     ***********************************************************
 */
 
-
-
-
 class Solution {
 public:
-    
+
     vector<int> dp;
-    
-    int fact(int n){
-        if(n==1 || n==0) return 1;
-        return fact(n-1)*n;
+
+    Solution(){
+        dp.resize(46,-1);
     }
-    
-    int df(int i, int n){
-        if(i > n) return 0;
-        if(i == n) return 1;
-        if(dp[i]!=-1) return dp[i];
-        int ans = 0;
-        ans += df(i+2,n);
-        ans += df(i+1,n);
-        return dp[i] = ans;
-    }
-    
+
     int climbStairs(int n) {
-        // int m = n/2;
-        int ans = 0;
-        // int e = n%2;
-        // for(int i = 0; i <= m; i++){
-        //     ans += fact((2*(m-i) + i + e )) / (fact(2*(m-i)+e) * fact(i)); 
-        //     // cout << 2*(m-i)+e << " "<< i <<"\n";
-        // }
-        // return ans;
-        dp.resize(n+1,-1);
-        return df(0,n);
+        if(n<0) return 0;
+        if(n==0) return 1;
+        if(dp[n] != -1) return dp[n];
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
     }
 };
