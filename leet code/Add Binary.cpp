987bb0c0e2f652da7f3ab021996ab926fcaa2ffd
rahul <rasuru04@gmail.com>
2023-02-14 12:20:43 +0530

    improved addbinary code feb 14th

--- leet code/Add Binary.cpp
+++ leet code/Add Binary.cpp
@@ -19,47 +19,32 @@ Given two binary strings a and b, return their sum as a binary string.
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string ans(max(a.length(),b.length())+5,'a');
-        int i = a.length()-1, j = b.length()-1, k = max(a.length(),b.length())+4;
+        int n = a.length()-1,m = b.length()-1;
+        string ans(max(n,m)+2,'a');
+        int k = max(n,m)+1;
         int c = 0;
-        while(i>=0 && j >= 0){
-            if(a[i] == '1' && b[j] == '1'){
-                ans[k] = '0' + c;
+        int i;
+        while(0 <= n && 0 <= m){
+            if(a[n]=='1' && b[m] == '1'){
+                ans[k] = c + '0';
                 c=1;
+            } else if(a[n]=='1' || b[m] == '1'){
+                ans[k] = (c^1) + '0';
+                // c=c^1;
             }
-            else if(a[i] == '1' && b[j] == '0' || a[i] == '0' && b[j] == '1'){
-                ans[k] = (c==0)? '1' : '0';
-                // c = (c==1)? 1 : 0;
-            }
-            else {
-                // if(c>1) c = 1;
-                ans[k] = '0'+c;
+            else{
+                ans[k] = c + '0';
                 c = 0;
             }
+            // cout << n<<" "<<m<<" "<<ans<<"\n";
+            n--;
+            m--;
             k--;
-            i--;
-            j--;
         }
-        if(i>=0){
-            while(i>=0){
-                if(a[i]=='0') { ans[k] = '0'+c; c = 0; }
-                else { ans[k] = ((c == 0) ? '1' : '0'); } 
-                k--;
-                i--;
-            }
-        }
-        if(j>=0){
-            while(j>=0){
-                if(b[j]=='0') { ans[k] = '0'+c; c = 0; }
-                else { ans[k] = ((c == 0) ? '1' : '0'); } 
-                k--;
-                j--;
-            }
-        }
-        if(c == 1) ans[k--] = '1';
-        int z = 0;
-        cout << ans <<" ";
-        while(z < max(a.length(),b.length())+5 && ans[z] == 'a') z++;
-        return ans.substr(z,max(a.length(),b.length())+5-z);
+        while(n>=0){ if(a[n] == '1') ans[k--] = (c^1)+'0'; else { ans[k--] = c+'0'; c = 0; } n--; }
+        while(m>=0){ if(b[m] == '1') ans[k--] = (c^1)+'0'; else { ans[k--] = c+'0'; c = 0; } m--; }
+        //   cout << n<<" "<<m<<" "<<c<<" "<<ans<<"\n";
+        if(c) { ans[k] = '1';  return ans;}   
+        return ans.substr(1);
     }
 };
