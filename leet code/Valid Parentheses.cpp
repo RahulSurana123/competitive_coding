be4d4c90b7f6c7788184db3ca98d3cb2775cea77
rahul <rasuru04@gmail.com>
2023-04-10 10:54:06 +0530

    updating daily code 10th april

--- leet code/Valid Parentheses.cpp
+++ leet code/Valid Parentheses.cpp
@@ -24,21 +24,21 @@ Open brackets must be closed in the correct order.
 
 class Solution {
 public:
-    bool isValid(string s) {
+    bool isValid(string &s) {
         stack<char> st;
-        for(int i = 0; i< s.length(); i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+        for(int i = 0; i < s.length(); i++){
+            if(s[i]=='(' || s[i]=='{' || s[i] == '['){
                 st.push(s[i]);
             }
             else{
-                if(st.empty()) return false;
-                if(s[i] == ')' && st.top() == '(') st.pop();
-                else if(s[i] == '}' && st.top() == '{')st.pop();
-                else if(s[i] == ']' && st.top() == '[') st.pop();
-                else return false;
+                if(st.empty())return false;
+                char x = st.top();
+                st.pop();
+                if(x == '(' && s[i] != ')') return false;
+                if(x == '{' && s[i] != '}') return false;
+                if(x == '[' && s[i] != ']') return false;
             }
         }
-        if(st.empty()) return true;
-        return false;
+        return st.size() == 0? true:false;
     }
 };
