20e855872bd2de176cd1582ffc68214eaea9d93e
rahul <rasuru04@gmail.com>
2023-12-29 16:56:50 +0530

    updating daily code 29 Dec

--- leet code/Minimum Difficulty of a Job Schedule.cpp
+++ leet code/Minimum Difficulty of a Job Schedule.cpp
@@ -5,55 +5,47 @@
     
     ***********************************************************
 
+You want to schedule a list of jobs in d days. 
+Jobs are dependent (i.e To work on the ith job, you have to finish all the jobs j where 0 <= j < i).
 
-You want to schedule a list of jobs in d days. Jobs are dependent (i.e To work on the ith job, you have to finish all the jobs j where 0 <= j < i).
+You have to finish at least one task every day. 
+The difficulty of a job schedule is the sum of difficulties of each day of the d days. 
+The difficulty of a day is the maximum difficulty of a job done on that day.
 
-You have to finish at least one task every day. The difficulty of a job schedule is the sum of difficulties of each day of the d days. The difficulty of a day is the maximum difficulty of a job done on that day.
+You are given an integer array jobDifficulty and an integer d. 
+The difficulty of the ith job is jobDifficulty[i].
 
-You are given an integer array jobDifficulty and an integer d. The difficulty of the ith job is jobDifficulty[i].
-
-Return the minimum difficulty of a job schedule. If you cannot find a schedule for the jobs return -1.
-
+Return the minimum difficulty of a job schedule. 
+If you cannot find a schedule for the jobs return -1.
 
 
     ***********************************************************
 */
 
+class Solution {
+public:
 
-#include <bits/stdc++.h>
-#define ll          long long
-#define vl          vector<ll>
-#define vi          vector<int>
-#define pi          pair<int,int>
-#define pl          pair<ll,ll>
-#define all(a)      a.begin(),a.end()
-#define mem(a,x)    memset(a,x,sizeof(a))
-#define pb          push_back
-#define mp          make_pair
-#define F           first
-#define S           second
-#define FOR(i,a)     for(int i = 0; i < a; i++)
-#define trace(x)    cerr<<#x<<" : "<<x<<endl;
-#define trace2(x,y) cerr<<#x<<" : "<<x<<" | "<<#y<<" : "<<y<<endl;
-#define trace3(x,y,z) cerr<<#x<<" : "<<x<<" | "<<#y<<" : "<<y<<" | "<<#z<<" : "<<z<<endl;
-#define fast_io 	std::ios::sync_with_stdio(false),cin.tie(NULL),cout.tie(NULL)
- 
-using namespace std;
+    vector<vector<int>> dp;
 
-// vector<vector<int>> adj;
-// int ans = 0;
+    int df(int d, int i, vector<int>& jD){
+        if(i == jD.size() && d == 0) return 0;
+        if(i >= jD.size()) return 1e9;
+        if(d <= 0) return 1e9;
 
-int main()
-{
-	fast_io;
-    int t=1;
-    // cin >> t;
-    while(t--) {
-        int n,k;
-        cin >> n >> k;
-        // dp.clear();
-        // adj.resize(n+1,vector<>());
-        // dp.resize(n,-1);
-        cout << ans <<"\n";
+        if(dp[d][i] != -1) return dp[d][i];
+
+        int x = 1e9;
+        int ma = 0;
+        for(int j = i; j < jD.size(); j++){
+            ma = max(ma,jD[j]);
+            x = min(x, ma + df(d-1,j+1,jD));
+        }
+        return dp[d][i] = x;
     }
-}
+
+    int minDifficulty(vector<int>& jobDifficulty, int d) {
+        dp.resize(d+1,vector<int>(jobDifficulty.size(),-1));
+        int ans = df(d,0,jobDifficulty);
+        return ans >= 1e9 ? -1:ans;
+    }
+};
