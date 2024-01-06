0b4b0e8fb60126cdbbbed79e80b2c343ffc14373
Rahul Surana <rasuru04@gmail.com>
2024-01-06 22:15:28 +0530

    updating daily code 06 Jan Readme Update

--- commit.sh
+++ commit.sh
@@ -1,5 +1,5 @@
 git add .
-read -p "enter the extra message" msg
+read -p "enter the extra message : " msg
 git commit -m " updating daily code `date +\"%d %b\"` $msg "
 git push origin main
 echo "Done with the script"
