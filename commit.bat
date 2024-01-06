dea1b0fc3438ceadf0aa3de924bb6b0aa8aafc3a
Rahul Surana <rasuru04@gmail.com>
2024-01-06 22:52:58 +0530

    updating daily code 06-JAN

--- commit.bat
+++ commit.bat
@@ -1,6 +1,17 @@
 git add .
-read -p "enter the extra message : " msg
-git commit -m " updating daily code `date +\"%d %b\"` $msg "
+echo " Enter the message : "
+set -p msg
+
+setlocal
+for /f %%I in ('wmic os get localdatetime /format:list ^| find "="') do set "%%I"
+set "YYYY=%localdatetime:~0,4%"
+set /a "MM=1%localdatetime:~4,2% - 100"
+set "DD=%localdatetime:~6,2%"
+for /f "tokens=%MM%" %%I in ("JAN FEB MAR APR MAY JUN JUL AUG SEP OCT NOV DEC") do set "month=%%I"
+
+echo %DD%-%month%-%YYYY%
+
+git commit -m " updating daily code %DD%-%month% %msg% "
 git push origin main
-echo "Done with the script"
-read -p "exiting " u
+echo "Done with the script \n exiting"
+set -p u
