4417d9458454b73919e10aca964610e2e3b1dc3f
rahul <rasuru04@gmail.com>
2024-01-08 11:08:38 +0530

    updating daily code 08 Jan Daily Improvement

--- commit.sh
+++ commit.sh
@@ -3,6 +3,6 @@ git add .
 read -p "enter the extra message : " msg
 ./.git/hooks/pre-commit
 git commit -m " updating daily code `date +\"%d %b\"` $msg "
-git push origin main
+git push origin RahulSurana
 echo "Done with the script"
 read -p "exiting " u
