6163cb6c73f62714a2bf02319b94eb12baa0c6f8
rahul <rasuru04@gmail.com>
2024-01-14 18:29:57 +0530

    14 Jan Updating Readme

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 git add .
 read -p "enter the extra message : " msg
 ./.git/hooks/pre-commit
-git commit -m " updating daily code `date +\"%d %b\"` $msg "
+git commit -m " `date +\"%d %b\"` $msg "
 git push origin main
 echo "Done with the script"
 read -p "exiting " u
