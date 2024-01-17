8fce16ba8397a68cc8db31c53e0ee90ff72ec360
rahul <rasuru04@gmail.com>
2024-01-17 11:51:35 +0530

    17 Jan Improving Code

--- leet code/Unique Number of Occurrences.cpp
+++ leet code/Unique Number of Occurrences.cpp
@@ -19,13 +19,16 @@ return true if the number of occurrences of each value in the array is unique, o
 
 class Solution {
 public:
-    
-    unordered_map<int,int> uo;
-    
     bool uniqueOccurrences(vector<int>& arr) {
-        for(auto x: arr) uo[x]++;
-        set<int> a;
-        for(auto y: uo){ if(a.count(uo[y.first])) return false; a.insert(y.second); }
+        unordered_map<int,int> m;
+        sort(arr.begin(),arr.end());
+        int n = arr.size(), c = 1;
+        for(int i = 1; i < n; i++){
+            if(arr[i-1] == arr[i]) c++;
+            else { m[c]++; if(m[c]>1) return false; c = 1; }
+        }
+        if(m[c] == 1) return false;
         return true;
-    } 
+        
+    }
 };
