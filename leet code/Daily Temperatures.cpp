3bb57db5ee1b1985fccb6a613c5b33ecfb671d05
rahul <rasuru04@gmail.com>
2024-01-31 15:52:22 +0530

    31 Jan Improving Code

--- leet code/Daily Temperatures.cpp
+++ leet code/Daily Temperatures.cpp
@@ -25,11 +25,10 @@ public:
         int n = temperatures.size();
         vector<int> ans(n,0);
         stack<int> s;
-        for(int i = n-1; i>= 0;i--){
-            while(!s.empty() && temperatures[s.top()]<=temperatures[i]) s.pop();
+        for(int i = n-1; i >= 0; i--){
+            while(!s.empty() && temperatures[s.top()] <= temperatures[i]) s.pop();
             if(!s.empty())
             ans[i] = s.top()-i;
-            else ans[i] = 0;
             s.push(i);
         }
         return ans;
