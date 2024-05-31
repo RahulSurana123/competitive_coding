30cfbe7b5ffa33e49c6234d9170a7a5ef4b13e0c
rahul <rasuru04@gmail.com>
2024-05-31 21:14:06 +0530

    31 May Updating Daily Code

--- leet code/Single Number III.cpp
+++ leet code/Single Number III.cpp
@@ -22,11 +22,20 @@ You must write an algorithm that runs in linear runtime complexity and uses only
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) {
-        set<int> x;
-        for(auto z: nums){
-            if(x.count(z)) x.erase(z);
-            else x.insert(z);
+        if(nums.size() == 2) return nums;
+        int e = 0,m = 1e9;
+        bool u = false;
+        sort(nums.begin(),nums.end());
+        for(int i = 1; i < nums.size(); i+=2) { 
+            e ^= nums[i];
+            e ^= nums[i-1];
+            if(nums[i] != nums[i-1]){ 
+                if(!u) { 
+                    m = nums[i-1]; 
+                    u = true;
+                }
+            } 
         }
-        return vector<int>(x.begin(),x.end());
+        return {m,e^m};
     }
 };
